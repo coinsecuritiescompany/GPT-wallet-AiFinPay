5332773fcaf1c64b400897f139cc2a2dcb08d61a
coinsecuritiescompany <coinsecuritiescompany@gmail.com>
2026-07-31 01:19:53 +0300

    Export NEAR transfer codec

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./amounts.js";
 export * from "./constants.js";
 export * from "./errors.js";
+export * from "./near-transfer.js";
 export * from "./schemas.js";
 export * from "./solana-transfer.js";
 export * from "./types.js";
